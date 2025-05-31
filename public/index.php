1c10c3639018dc3801888108da59500408d495d6
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2025-06-01 01:23:26 +0600

    fix(public):
    - Modified 1 file: index.php
    - Main directories: public, Validator.php -> Core

--- public/index.php
+++ public/index.php
@@ -2,9 +2,17 @@
 
 const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'functions.php';
+//require base_path('Response.php');
+//require base_path('Database.php');
 
-require base_path('Response.php');
-require base_path('Database.php');
+spl_autoload_register(static function ($class) {
+	$file = base_path("Core/" . $class . '.php');
+	if (file_exists($file)) {
+		require $file;
+	}
+});
+
+
 require base_path('router.php');
 
 
